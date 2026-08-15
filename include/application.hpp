e69ea04e5d763c5f5943562b7cc166159e2e5a9f
Vladimir <56470379+belenkiy-lab@users.noreply.github.com>
2026-08-15 20:15:50 +0300

    Use LittleFS in application

--- include/application.hpp
+++ include/application.hpp
@@ -5,7 +5,8 @@
 #include <ArduinoJson.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
-#include <ESP8266FtpServer.h>
+#include <LittleFS.h>
+#include <SimpleFTPServer.h>
 #include <WebSocketsServer.h>
 #include <DNSServer.h>
 #include <Ticker.h>
