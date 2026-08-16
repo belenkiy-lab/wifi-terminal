7da2b26aea9205afd556f807ec9bdd6ef9a734fe
Vladimir <56470379+belenkiy-lab@users.noreply.github.com>
2026-08-17 01:26:46 +0300

    Add WiFi client settings

--- include/jsonconfig.hpp
+++ include/jsonconfig.hpp
@@ -14,16 +14,22 @@
 #define MAX_APSSID_LEN 32
 #define MIN_PASS_LEN 8
 #define MAX_PASS_LEN 64
+#define MAX_WIFI_SSID_LEN 32
+#define MIN_WIFI_PASS_LEN 8
+#define MAX_WIFI_PASS_LEN 64
 #define AP_MAX_CHANNEL 13
 const unsigned int DEFAULT_SERIAL_CONFIG = SERIAL_8N1;
 static const char DEFAULT_AP_SSID[] PROGMEM = "WirelessTerminal";
 static const char DEFAULT_AP_PASS[] PROGMEM = "123456789";
+static const char WIFI_PASSWORD_MASK[] PROGMEM = "********";
 static const char HTML_ID_SBAUD[] PROGMEM = "baud";
 static const char HTML_ID_SCONFIG[] PROGMEM = "config";
 static const char HTML_ID_APSSID[] PROGMEM = "SSID";
 static const char HTML_ID_APPASS[] PROGMEM = "password";
 static const char HTML_ID_APCHANNEL[] PROGMEM = "channel";
 static const char HTML_ID_APADDRESS[] PROGMEM = "address";
+static const char HTML_ID_WIFI_SSID[] PROGMEM = "wifi_ssid";
+static const char HTML_ID_WIFI_PASSWORD[] PROGMEM = "wifi_password";
 
 class Configuration
 {
@@ -35,6 +41,9 @@ public:
     String APPassword = FPSTR(DEFAULT_AP_PASS);
     unsigned int APchannel = DEFAULT_AP_CHANNEL;
     IPAddress APaddress = IPAddress(192, 168, 4, 1);
+
+    String WiFiSSID;
+    String WiFiPassword;
 
     void serialize(DynamicJsonDocument &document);
     void deserialize(DynamicJsonDocument &document);
